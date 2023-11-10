7a1049581f06bb3e2201a8f3741a496e7e0d02bc
Muhammad <auwalmuhd365@gmail.com>
2023-11-10 09:51:20 +0100

    add dark mode functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from './components/Main'
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen bg-slate-50 dark:bg-slate-900 dark:text-slate-50">
+    <div className="flex flex-col min-h-screen select-none bg-slate-50 dark:bg-slate-900 dark:text-slate-50">
       <Header />
       <Main />
     </div>
