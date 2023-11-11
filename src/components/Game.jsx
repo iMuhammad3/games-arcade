ca658f5d0665d0cba834fdf3d1cebbe9b0d782a9
Muhammad <auwalmuhd365@gmail.com>
2023-11-11 04:40:30 +0100

    make responsive

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,7 +1,7 @@
 function Game(){
     return (
-        <div className="rounded-md overflow-hidden shadow-lg">
-            <img className="w-96 aspect-video object-cover" src="./src/assets/game-previews/typing-game.png" alt="" />
+        <div className="rounded-md overflow-hidden shadow-lg dark:shadow-gray-600/20 cursor-pointer hover:-translate-y-1 transition">
+            <img className="w-[350px] aspect-video object-cover" src="./src/assets/game-previews/typing-game.png" alt="" />
             <p className="px-8 py-4 md:text-lg dark:bg-slate-950">Typing Game</p>
         </div>
     )
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,17 @@
 import Game from "./Game";
+import SearchInput from "./Input";
 
 function Main() {
-    return <main className="py-10 px-16">
-        <ul className="flex gap-6">
-        <Game />
-        <Game />
-        <Game />
-        </ul>
-    </main>;
+    return (
+        <main className="py-10 px-16 flex flex-col gap-6">
+            <SearchInput forHeader={false} />
+            <ul className="flex justify-center flex-wrap gap-6">
+                <Game />
+                <Game />
+                <Game />
+            </ul>
+        </main>
+    );
 }
 
 export default Main;
