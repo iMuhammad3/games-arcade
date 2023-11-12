62f03aeab9f1e2ac27046c58063520f12909d6d5
Muhammad <auwalmuhd365@gmail.com>
2023-11-12 06:46:00 +0100

    download fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
       colors: {
         "slate-850": "rgb(27,37,55)",
         "slate-950": "rgb(7,16,40)",
+      },
+      fontFamily: {
+        "sans": ["Poppins", "sans-serif"]
       }
     }
   },
