2a64c788240557bfd4213c391132c2129afc39ce
Muhammad <auwalmuhd365@gmail.com>
2023-11-14 21:55:35 +0100

    add color logic

--- src/games/typing-game/Typing.jsx
+++ src/games/typing-game/Typing.jsx
@@ -1,26 +1,53 @@
-import { useEffect, useState } from "react";
+import { Children, useEffect, useState } from "react";
 import Timer from "./Timer";
 
-export default function Game(){
-    const [text, setText] = useState("")
-    
+export default function Game() {
+    const [text, setText] = useState([]);
+    const [userInput, setUserInput] = useState([])
+
     useEffect(() => {
         const fetchQuote = async () => {
-            const resp = await fetch("http://api.quotable.io/random")
-            const data = await resp.json()
+            const resp = await fetch("http://api.quotable.io/random");
+            const data = await resp.json();
             console.log(data);
-            setText(data.content)
-        }
-        fetchQuote()
-    }, [])
+            setText(data.content.split(""));
+        };
+        fetchQuote();
+    }, []);
+
+    const handleInput = (e) => {
+        const newUserInput = e.target.value.split("")
+        setUserInput(newUserInput)
+        console.log(userInputArray)
+    }
 
     return (
         <div className="flex flex-col items-center relative">
             <Timer />
             <div className="bg-slate-200 dark:bg-slate-700 md:w-[450px] w-80 mt-16 p-4 flex flex-col gap-2 rounded-md shadow-lg">
-                <p>{text}</p>
-                <textarea rows="5" className="bg-slate-100 dark:bg-slate-800 px-4 py-2 w-full rounded outline-none resize-none"></textarea>
+                <p>
+                    {text.map((char, index) => (
+                        <Char correct={
+                            // check if user input at text index exists.
+                            // if so set value of 'correct' to result of 'userInput[index] === char'
+                            // else set to undefined
+                            userInput[index] ? userInput[index] === char : undefined
+                        } char={char} />
+                    ))}
+                </p>
+                <textarea
+                    onChange={handleInput}
+                    rows="5"
+                    className="bg-slate-100 dark:bg-slate-800 px-4 py-2 w-full rounded outline-none resize-none"
+                ></textarea>
             </div>
         </div>
-    )
+    );
+}
+
+function Char({char, correct}){
+    let color = "text-red-500 underline"
+    if(correct === undefined) color = "text-slate-100"
+    if(correct) color = "text-green-500"
+    return <span className={color}>{char}</span>
 }
