d938915a0ac3f16a8dd3eedb66e0e9775fc56ab3
Muhammad <auwalmuhd365@gmail.com>
2023-11-20 14:46:05 +0100

    remove blue color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,6 @@ module.exports = {
       colors: {
         "slate-850": "rgb(27,37,55)",
         "slate-950": "rgb(7,16,40)",
-        "midnightblue": "#030720",
       },
       fontFamily: {
         "sans": ["Poppins", "sans-serif"]
