0a03a96cd220ff608617d6efcb75b6f8169fe1c6
Muhammad <auwalmuhd365@gmail.com>
2023-11-26 13:39:38 +0100

    darken button color

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -5,7 +5,7 @@ const Button = ({ className = "", children, onClick }) => {
         <button
             onClick={onClick}
             className={
-                "px-4 py-2 rounded-md shadow-md bg-sky-600 text-sky-100 hover:brightness-125 " +
+                "px-4 py-2 rounded-md shadow-md bg-sky-700 text-sky-100 hover:brightness-125 " +
                 className
             }
         >
