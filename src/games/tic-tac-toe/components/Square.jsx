f14dbb8105f772d02d6e58eeb6df7883516bb106
Muhammad <auwalmuhd365@gmail.com>
2023-11-26 15:21:45 +0100

    fix responsive issues

--- src/games/tic-tac-toe/components/Square.jsx
+++ src/games/tic-tac-toe/components/Square.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export const Square = ({onClick, turn }) => {
-    return <div onClick={onClick} className={turn + " relative w-40 aspect-square border-2 cursor-pointer"}></div>;
+    return <div onClick={onClick} className={turn + " relative aspect-square border-2 cursor-pointer"}></div>;
 };
