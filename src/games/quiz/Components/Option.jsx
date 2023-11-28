ac0aa1ee3e338f4ee66400fab76728b3946c49a9
Muhammad <auwalmuhd365@gmail.com>
2023-11-28 14:58:34 +0100

    finish basics

--- src/games/quiz/Components/Option.jsx
+++ src/games/quiz/Components/Option.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
-export const Option = ({children, onClick, index}) => {
-  return (
-    <li className='cursor-pointer px-3 py-1 rounded hover:bg-slate-600 transition' onClick={onClick}>{index}. {children}</li>
-  )
-}
+export const Option = ({ children, onClick, index }) => {
+    return (
+        <li
+            className="cursor-pointer px-3 py-1 rounded hover:bg-slate-600 transition"
+            onClick={onClick}
+        >
+            {index}. {children}
+        </li>
+    );
+};
--- src/games/quiz/Game.jsx
+++ src/games/quiz/Game.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { ThreeDots } from "react-loader-spinner";
 import { Option } from "./Components/Option";
+import Button from "../../components/Buttons/Button";
 
 const API_URL =
     "https://opentdb.com/api.php?amount=8&difficulty=easy&type=multiple";
 
 const Game = () => {
     const [quiz, setQuiz] = useState([]);
+    const [score, setScore] = useState(0);
     const [currentQuiz, setCurrentQuiz] = useState(0);
+    const [finished, setFinished] = useState(false);
     const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
@@ -21,10 +24,28 @@ const Game = () => {
 
     const options = [
         quiz[currentQuiz]?.correct_answer,
-        ...quiz[currentQuiz]?.incorrect_answers,
+        ...(quiz[currentQuiz]?.incorrect_answers || []),
     ].sort(() => Math.random() - 0.5);
+ 
+    const nextQuiz = index => {
+        if (options[index] === quiz[currentQuiz]?.correct_answer) {
+            setScore(prev => prev + 1);
+        }
+        if (currentQuiz === quiz.length - 1) {
+            setFinished(true)
+            return
+        }
+        setCurrentQuiz(prev => prev + 1);
+    };
+
+    const restartQuiz = () => {
+        setFinished(false)
+        setCurrentQuiz(0) 
+        setScore(0)
+    }
 
     console.log(quiz);
+    console.log(options);
 
     return (
         <div className="center-game">
@@ -32,25 +53,40 @@ const Game = () => {
                 <header className="bg-slate-800 p-3 text-center text-2xl">
                     General Knowledge
                 </header>
-                {loaded ? (
-                    <div className="p-4">
-                        <h2>Question {currentQuiz + 1} of {quiz?.length}</h2>
-                        <div className="my-3 flex flex-col gap-2">
-                            <p>{quiz[currentQuiz]?.question}</p>
-                            <h3>Options:</h3>
-                            <ul className=" flex flex-col gap-1">
-                                {options.map((option, index) => {
-                                    return (
-                                        <Option key={index} index={index + 1}>
-                                            {option}
-                                        </Option>
-                                    );
-                                })}
-                            </ul>
-                        </div>
+                {finished ? (
+                    <div className="flex flex-col items-center justify-center gap-1 ">
+                        <h1 className="text-3xl">Your score is {score}</h1>
+                        <Button onClick={restartQuiz}>Restart</Button>
                     </div>
                 ) : (
-                    <ThreeDots />
+                    <>
+                        {loaded ? (
+                            <div className="p-4">
+                                <h2>
+                                    Question {currentQuiz + 1} of {quiz?.length}
+                                </h2>
+                                <div className="my-3 flex flex-col gap-2">
+                                    <p>{quiz[currentQuiz]?.question}</p>
+                                    <h3>Options:</h3>
+                                    <ul className=" flex flex-col gap-1">
+                                        {options.map((option, index) => {
+                                            return (
+                                                <Option
+                                                    onClick={() => nextQuiz(index)}
+                                                    key={index}
+                                                    index={index + 1}
+                                                >
+                                                    {option}
+                                                </Option>
+                                            );
+                                        })}
+                                    </ul>
+                                </div>
+                            </div>
+                        ) : (
+                            <ThreeDots />
+                        )}
+                    </>
                 )}
             </div>
         </div>
