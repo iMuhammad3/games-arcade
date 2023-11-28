aa664138164bb387db7eb2bb606f065ccb14adb5
Muhammad <auwalmuhd365@gmail.com>
2023-11-28 20:36:34 +0100

    remove dark class

--- src/games/typing-game/components/Character.jsx
+++ src/games/typing-game/components/Character.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 export const Character = ({ char, correct }) => {
     let color = "text-red-500 underline";
     // if correct is undefined then leave the text as white
-    if (correct === undefined) color = "dark:text-slate-100 text-midnightblue";
+    if (correct === undefined) color = "text-midnightblue";
     if (correct) color = "text-green-500";
     return <span className={color}>{char}</span>;
 }
