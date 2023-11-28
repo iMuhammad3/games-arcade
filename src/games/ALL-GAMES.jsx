80160c17d1f3835c3b7308fe085a0dcf697a4d73
Muhammad <auwalmuhd365@gmail.com>
2023-11-28 18:02:01 +0100

    store assets in firebase storage

--- src/games/ALL-GAMES.jsx
+++ src/games/ALL-GAMES.jsx
@@ -1,72 +1,68 @@
-import React from 'react'
+import React from "react";
 
-import TypingGame from './typing-game/Game'
-import TypingVid from '../assets/game-videos/typing.mp4'
-import TypingImg from '../assets/game-images/typing.png'
+import TypingGame from "./typing-game/Game";
 
-import WhacGame from './whac-a-messi/Game'
-import WhacVid from '../assets/game-videos/whac-a-messi.mp4'
-import WhacImg from '../assets/game-images/whac-a-messi.png'
+import WhacGame from "./whac-a-messi/Game";
 
-import TicTacToe from './tic-tac-toe/Game'
-import TicVid from '../assets/game-videos/tic-tac-toe.mp4'
-import TicImg from '../assets/game-images/tic-tac-toe.png'
+import TicTacToe from "./tic-tac-toe/Game";
 
-import RockGame from './rock-paper-scissors/Game'
-import RockVid from '../assets/game-videos/rock-paper-scissors.mp4'
-import RockImg from '../assets/game-images/rock-paper-scissors.png'
+import RockGame from "./rock-paper-scissors/Game";
 
-import Memory from './memory-game/Game'
-import MemoryVid from '../assets/game-videos/memory.mp4'
-import MemoryImg from '../assets/game-images/memory.png'
+import Memory from "./memory-game/Game";
 
-import Quiz from './quiz/Game'
-import QuizVid from '../assets/game-videos/quiz.mp4'
-import QuizImg from '../assets/game-images/quiz.png'
+import Quiz from "./quiz/Game";
+
+const getImage = (name, type = "png") => {
+    return `https://firebasestorage.googleapis.com/v0/b/mask-games.appspot.com/o/images%2Fgame-previews%2F${name}.${type}?alt=media&token=e6ff09fc-2bb5-4ca0-9090-93da1ca7bfd1`;
+};
+
+const getVideo = (name, type = "mp4") => {
+    return `https://firebasestorage.googleapis.com/v0/b/mask-games.appspot.com/o/videos%2F${name}.${type}?alt=media&token=3fe17cf3-3965-41a1-9aef-f88b18c62a1d`
+}
 
 const GAMES = [
     {
         id: 1,
         name: "typing-game",
         element: <TypingGame />,
-        video_url: TypingVid,
-        img_url: TypingImg,
+        video_url: getVideo("typing"),
+        img_url: getImage('typing'),
     },
     {
         id: 2,
         name: "whac-a-messi",
         element: <WhacGame />,
-        video_url: WhacVid,
-        img_url: WhacImg,
+        video_url: getVideo("whac-a-messi"),
+        img_url: getImage("whac-a-messi"),
     },
     {
         id: 3,
         name: "tic-tac-toe",
         element: <TicTacToe />,
-        video_url: TicVid,
-        img_url: TicImg,
+        video_url: getVideo("tic-tac-toe"),
+        img_url: getImage("tic-tac-toe"),
     },
     {
         id: 4,
         name: "rock-paper-scissors",
         element: <RockGame />,
-        video_url: RockVid,
-        img_url: RockImg,
+        video_url: getVideo("rock-paper-scissors"),
+        img_url: getImage("rock-paper-scissors"),
     },
     {
         id: 5,
         name: "memory-game",
         element: <Memory />,
-        video_url: MemoryVid,
-        img_url: MemoryImg,
+        video_url: getVideo("memory"),
+        img_url: getImage("memory"),
     },
     {
         id: 6,
         name: "quiz",
         element: <Quiz />,
-        video_url: QuizVid,
-        img_url: QuizImg,
+        video_url: getVideo("quiz"),
+        img_url: getImage("quiz"),
     },
-]
+];
 
-export default GAMES
+export default GAMES;
