f236680502f023c84960b1e23fe24693c8bc11ea
Muhammad <auwalmuhd365@gmail.com>
2023-11-30 21:24:21 +0100

    add animate.css

--- src/pages/Home/Main.jsx
+++ src/pages/Home/Main.jsx
@@ -1,21 +1,23 @@
 // Main.jsx
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "../../components/Buttons/Button";
-import animation from '../../assets/animation.gif'
+import "animate.css";
 
-function Main() {
+const Main = () => {
     const navigate = useNavigate();
     return (
         <main className="flex flex-col items-center flex-grow z-10 relative">
-            <h1 className="mt-10">
-                We've got games for you!
-            </h1>
+            <div className="flex flex-wrap justify-center gap-4 mt-10 mx-3 text-5xl">
+                <Word color="from-cyan-500 to-sky-500">Eat.</Word> 
+                <Word delay="animate__delay-1s" color="from-sky-500 to-blue-500">Sleep.</Word> 
+                <Word delay="animate__delay-2s" color="from-blue-500 to-indigo-500">Game.</Word>
+                <Word delay="animate__delay-3s" color="from-indigo-500 to-violet-500">Repeat...</Word>
+            </div>
             <div className="-mt-40 flex-grow flex flex-col justify-center">
-                <img className="-mt-20" src={animation} alt="" />
+                {/* <img className="-mt-20" src={animation} alt="" /> */}
                 <Button
-                    className="px-8 text-xl bg-gradient-to-tr from-sky-600 to-blue-600"
-                    // navigates to games page
+                    className="px-8 text-xl bg-gradient-to-tr from-sky-600 to-blue-600 "
                     onClick={() => navigate("/games")}
                 >
                     Play Games!
@@ -23,6 +25,14 @@ function Main() {
             </div>
         </main>
     );
-}
+};
+
+const Word = ({ children, delay, color }) => {
+    return (
+        <div className={`bg-gradient-to-r text-transparent bg-clip-text inline-block animate__animated animate__rubberBand ${delay} ${color}`}>
+            {children}
+        </div>
+    );
+};
 
 export default Main;
