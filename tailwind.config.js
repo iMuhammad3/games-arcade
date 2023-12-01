4ab097754071fa6d2128450d31cfe61b3b233851
Muhammad <auwalmuhd365@gmail.com>
2023-12-01 08:18:19 +0100

    darken nightblue-900

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
       colors: {
         "nightblue-700": "hsl(218.29,45%,23.67%)",
         "nightblue-800": "hsl(222.22,49.09%,10.78%)",
-        "nightblue-900": "hsl(222.86,48.84%,8.43%)",
+        "nightblue-900": "hsl(224.86,61.84%,6.43%)",
       },
       fontFamily: {
         "sans": ["Poppins", "sans-serif"],
