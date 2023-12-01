a61f5a1fe265eb5c04f4e4a6a6fdb47034de2f72
Muhammad <auwalmuhd365@gmail.com>
2023-12-01 08:02:20 +0100

    remove useless dark class

--- src/games/typing-game/Game.jsx
+++ src/games/typing-game/Game.jsx
@@ -67,14 +67,14 @@ const Game = () => {
   const GameUI = (
     <>
       <Timer />
-      <div className="bg-slate-200 dark:bg-slate-750 md:w-[550px] w-full mx-2 md:mx-0 p-4 flex flex-col gap-4 rounded-md shadow-lg">
+      <div className="bg-slate-800 md:w-[550px] w-full mx-2 md:mx-0 p-4 flex flex-col gap-4 rounded-md shadow-lg">
         <p className="text-lg">{characters()}</p>
         <textarea
           ref={textareaRef}
           value={userInput.join("")}
           onChange={handleInput}
           rows="7"
-          className="bg-slate-850 font-serif px-4 py-2 w-full rounded outline-none resize-none"
+          className="bg-nightblue-800 font-serif px-4 py-2 w-full rounded outline-none resize-none"
         ></textarea>
       </div>
     </>
