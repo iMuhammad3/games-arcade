8c75c7c1bf45a60e1c1dea742b8c86a8882240b5
Muhammad <auwalmuhd365@gmail.com>
2023-12-02 11:01:30 +0100

    use Select component

--- src/pages/Games/Categories.jsx
+++ src/pages/Games/Categories.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Select } from "../../components/select/Select";
 export const Categories = () => {
-  return (
-    <select className="bg-nightblue-700 px-5 py-2 rounded-lg cursor-pointer outline-none">
-        <option selected>All Games</option>
-        <option value="Fun" >Fun</option>
-        <option value="Educational" >Educational</option>
-        <option value="Desktop" >Desktop/Laptop</option>
-        <option value="Others" >Others</option>
-    </select>
-  )
-}
+    return (
+        <Select>
+            <option selected>All Games</option>
+            <option value="Fun">Fun</option>
+            <option value="Educational">Educational</option>
+            <option value="Desktop">Desktop/Laptop</option>
+            <option value="Others">Others</option>
+        </Select>
+    );
+};
