6b8e99943dc1307131caa2cf9d64deafee3f1ca8
Muhammad <auwalmuhd365@gmail.com>
2023-12-02 12:57:51 +0100

    add animation

--- src/components/modals/GameModal.jsx
+++ src/components/modals/GameModal.jsx
@@ -14,10 +14,10 @@ export const GameModal = ({ children, setIsModal, isModal }) => {
                     id="modal"
                     onClick={closeModal}
                     className={
-                        "absolute inset-0 flex justify-center items-center overflow-hidden bg-nightblue-900/90 z-50"
+                        " absolute inset-0 flex justify-center items-center overflow-hidden bg-nightblue-900/90 z-50"
                     }
                 >
-                    <article className="flex flex-col gap-3 relative p-3 mx-4 w-full max-w-[400px] rounded shadow bg-nightblue-700">
+                    <article className="animate__animated animate__fadeInDown flex flex-col gap-3 relative p-3 mx-4 w-full max-w-[400px] rounded shadow bg-nightblue-700">
                         <span
                             onClick={() => setIsModal(false)}
                             className="text-xl font-light absolute top-3 right-4 cursor-pointer leading-4 p-1"
