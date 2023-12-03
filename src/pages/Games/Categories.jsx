6265684e14670b030e56188e91191b5d5bc59656
Muhammad <auwalmuhd365@gmail.com>
2023-12-03 09:19:55 +0100

    change value attribute to defaultValue

--- src/pages/Games/Categories.jsx
+++ src/pages/Games/Categories.jsx
@@ -3,7 +3,7 @@ import { Select } from "../../components/select/Select";
 export const Categories = () => {
     return (
         <Select>
-            <option selected>All Games</option>
+            <option defaultValue={true}>All Games</option>
             <option value="Fun">Fun</option>
             <option value="Educational">Educational</option>
             <option value="Desktop">Desktop/Laptop</option>
