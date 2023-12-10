5c2029c5fa00b3dbd1a71106cf45ae28d5cd1e43
Muhammad <auwalmuhd365@gmail.com>
2023-12-10 17:58:04 +0100

    hide sensitive data with .env

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDskha6PchTX8vAh-QHwE2DDD-y_iOgupU",
-    authDomain: "mask-games.firebaseapp.com",
-    projectId: "mask-games",
-    storageBucket: "mask-games.appspot.com",
-    messagingSenderId: "575616832659",
-    appId: "1:575616832659:web:89d0c150e4fa9a95eea966",
-    measurementId: "G-29T6GJ5CFX",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
