a81772a7a31a1be7995628eeaca029c8ae1a51e3
Muhammad <auwalmuhd365@gmail.com>
2023-12-14 13:28:43 +0100

    format

--- src/games/useGames.jsx
+++ src/games/useGames.jsx
@@ -12,33 +12,30 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../config/firebase.config";
 
 const elements = {
-    "typing-game" : <TypingGame />,
-    "memory-game" : <Memory />,
-    "quiz" : <Quiz />,
-    "rock-paper-scissors" : <RPS />,
-    "whac-a-messi" : <WhacGame />,
-    "tic-tac-toe" : <TicTacToe />,
-}
+    "typing-game": <TypingGame />,
+    "memory-game": <Memory />,
+    "quiz": <Quiz />,
+    "rock-paper-scissors": <RPS />,
+    "whac-a-messi": <WhacGame />,
+    "tic-tac-toe": <TicTacToe />,
+};
 
 export const useGames = () => {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState([]);
 
     useEffect(() => {
         const fetchGames = async () => {
             const querySnapshot = await getDocs(collection(db, "games"));
             const data = querySnapshot.docs.map(doc => {
-                return { 
+                return {
                     ...doc.data(),
-                    element: elements[doc.data().name]
+                    element: elements[doc.data().name],
                 };
             });
-            setGames(data)
+            setGames(data);
         };
-        fetchGames()
-    }, [])
-    
-  return games
-}
+        fetchGames();
+    }, []);
 
-
-
+    return games;
+};
